acbf5b0287753a3f14787335ddcfbabb36f9e251
Scott Sperling <scsper@gmail.com>
2014-11-01 23:00:40 -0700

    install node js and go properly

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install make
-apt-get -y install vim
-apt-get -y install git-core
-apt-get -y install nodejs
+sudo apt-get update
+sudo apt-get -y install python-software-properties
+
+sudo apt-get -y install make
+sudo apt-get -y install curl
+sudo apt-get -y install vim
+sudo apt-get -y install git-core
+
+sudo apt-add-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get -y install nodejs
+
+wget https://storage.googleapis.com/golang/go1.3.3.linux-386.tar.gz
+sudo tar -C /usr/local -xzf go1.3.3.linux-386.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
