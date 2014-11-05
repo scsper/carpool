13ae06c0dcf13ff745964c1b5d19ffdb222349e7
Scott Sperling <scsper@gmail.com>
2014-11-04 20:01:38 -0800

    postgres with utf8 encoding

--- install/install.sh
+++ install/install.sh
@@ -8,13 +8,19 @@ sudo apt-get -y install curl
 sudo apt-get -y install vim
 sudo apt-get -y install git-core
 
-sudo apt-add-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get -y install nodejs
+# sudo apt-add-repository ppa:chris-lea/node.js
+# sudo apt-get update
+# sudo apt-get -y install nodejs
 
-wget https://storage.googleapis.com/golang/go1.3.3.linux-386.tar.gz
-sudo tar -C /usr/local -xzf go1.3.3.linux-386.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+# wget https://storage.googleapis.com/golang/go1.3.3.linux-386.tar.gz
+# sudo tar -C /usr/local -xzf go1.3.3.linux-386.tar.gz
+# echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
-sudo apt-get -y install postgresql
-sudo apt-get -y install postgresql-contrib
+sudo apt-get install -y postgresql libpq-dev
+sudo su - postgres -c 'createuser -s vagrant'
+sudo rm /etc/postgresql/9.1/main/pg_hba.conf
+sudo cp /vagrant/install/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
+sudo chown postgres:postgres /etc/postgresql/9.1/main/pg_hba.conf
+sudo service postgresql restart
+sudo su - postgres -c 'psql < /vagrant/install/pg_encoding_fix.sql'
+sudo -u postgres createdb vagrant
