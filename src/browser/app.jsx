b8e86b529038895f16cbe95e06b45f16a238d2e1
Scott Sperling <scsper@gmail.com>
2015-06-08 18:56:19 -0700

    Add basic App component.

---
+++ src/browser/app.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var App;
+
+App = React.createClass({
+    render: function() {
+        return (
+            <div>Hello world!</div>
+        );
+    }
+});
+
+module.exports = App;
