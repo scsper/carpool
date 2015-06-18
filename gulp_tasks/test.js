79afce639cf94bba71ae54d8e2caa2a9def68afd
Dmitrii Abramov <dabramov@yahoo-inc.com>
2015-06-17 22:43:05 -0700

    address PR comments

--- gulp_tasks/test.js
+++ gulp_tasks/test.js
@@ -3,5 +3,5 @@ var mocha = require('gulp-mocha');
 
 gulp.task('test', function() {
     gulp.src(['test/setup/unit.js', 'src/**/_unit_/*_test.js'])
-        .pipe(mocha({reporter: 'nyan'}));
+        .pipe(mocha({reporter: 'spec'}));
 });
