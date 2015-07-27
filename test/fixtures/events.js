1b4e268e7b44e79548ed2df692120357e74d0e38
Scott Sperling <scsper@gmail.com>
2015-07-27 09:35:20 -0700

    Make fixtures make more sense.

--- test/fixtures/events.js
+++ test/fixtures/events.js
@@ -1,11 +1,11 @@
 module.exports = [{
-    name: 'theHOUSEoc',
+    name: 'College Group',
     date: '7/14/2015',
     time: '7:00pm',
     description: 'Come worship with the college group.',
     rideIds: [1, 2, 3]
 }, {
-    name: 'Park Victoria Baptist Church',
+    name: 'Sunday Morning Worship',
     date: '7/23/2015',
     time: '10:00am',
     description: 'Join us as we gather on Sunday morning.',
