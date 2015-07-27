736f4e400dc3fcfd1d9547aedcd46a3635aae3fb
Scott Sperling <scsper@gmail.com>
2015-07-27 15:40:04 -0700

    Add ride record.

--- src/browser/stores/ride.js
+++ src/browser/stores/ride.js
@@ -1,4 +1,5 @@
 var Fluxxor = require('fluxxor');
+var RideRecord = require('../records/ride.js');
 var EventConstants = require('../constants/event.js');
 
 var RideStore = Fluxxor.createStore({
@@ -21,7 +22,9 @@ var RideStore = Fluxxor.createStore({
     },
 
     _storeRides(payload) {
-        this.rides = payload.rides;
+        payload.rides.forEach(ride => {
+            this.rides.push(new RideRecord(rideData));
+        });
     }
 });
 
