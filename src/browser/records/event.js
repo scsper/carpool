54fd3b6ca6880aca97f29c33d6fcf5fbafed45c0
Scott Sperling <scsper@gmail.com>
2015-07-27 15:36:36 -0700

    Add records.

---
+++ src/browser/records/event.js
@@ -0,0 +1,9 @@
+export default class Event {
+    constructor(payload) {
+        this.name = payload.name;
+        this.date = payload.date;
+        this.time = payload.time;
+        this.description = payload.description;
+        this.rideIds = payload.rideIds;  // [] of ride ids
+    }
+}
