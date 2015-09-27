dfcc445e87c3d6cfc99cf133b74ecac787b7250d
Scott Sperling <scsper@gmail.com>
2015-09-26 17:14:35 -0700

    Add passengers API

---
+++ src/server/queries/rides_passengers.js
@@ -0,0 +1,25 @@
+import {query} from '../db';
+
+const getPassengers = (rideId) => {
+    return new Promise ((resolve, reject) => {
+        query(
+            'select userId from rides_passengers where rideId = ($1)',
+            [rideId]
+        ).then((result) => {
+            resolve(result.rows);
+        }).catch(reject);
+    })
+};
+
+const insert = (userId, rideId) => {
+    return new Promise ((resolve, reject) => {
+        query(
+            'insert into rides_passengers (userId, rideId) values ($1, $2) returning id',
+            [userId, rideId]
+        ).then(result => {
+            resolve(result.rows[0]);
+        }).catch(reject);
+    });
+};
+
+export default {getPassengers, insert};
