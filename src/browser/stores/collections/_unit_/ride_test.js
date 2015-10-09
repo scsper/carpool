27136e85e74dcf34f202df2afd32542ec68a8a75
Scott Sperling <scsper@gmail.com>
2015-10-09 00:35:26 -0700

    Add ride collection tests and docs.

---
+++ src/browser/stores/collections/_unit_/ride_test.js
@@ -0,0 +1,96 @@
+import RideCollection from '../ride';
+
+describe('stores/collections/ride', function() {
+    beforeEach(function() {
+        this.collection = new RideCollection();
+    });
+
+    describe('#get', function() {
+        it('returns the ride that has the given ride id', function() {
+            let id = 1;
+            let ride = {
+                dummy: 'ride'
+            };
+
+            this.collection.rides[id] = ride;
+
+            expect(this.collection.get(id)).to.deep.equal(ride);
+        });
+    });
+
+    describe('#addRides', function() {
+        it('adds raw rides and converts them into ride records', function() {
+            let rawRides = [{
+                arrivaltime: "2015-01-08T16:00:00.000Z",
+                departuretime: "2015-01-08T19:00:00.000Z",
+                driverid: 1,
+                eventid: 1,
+                id: 1,
+                notes: "ello moto",
+                passengers: [],
+                seats: 4
+            }];
+
+            this.collection.addRides(rawRides);
+
+            expect(this.collection.rides[1].id).to.equal(1);
+        });
+    });
+
+    describe('#addMembersToRide', function() {
+        it('adds all members to a ride when the member ids are strings', function() {
+            let rideId = 1;
+            let memberIds = ['1', '2'];
+
+            this.collection.rides[rideId] = {
+                passengers: []
+            };
+
+            this.collection.addMembersToRide(rideId, memberIds);
+
+            expect(this.collection.rides[rideId].passengers).to.deep.equal([1, 2]);
+        });
+
+        it('adds all members to a ride when the member ids are numbers', function() {
+            let rideId = 1;
+            let memberIds = [1, 2];
+
+            this.collection.rides[rideId] = {
+                passengers: []
+            };
+
+            this.collection.addMembersToRide(rideId, memberIds);
+
+            expect(this.collection.rides[rideId].passengers).to.deep.equal([1, 2]);
+        });
+    });
+
+    describe('#removeMembersFromRide', function() {
+        it('removes all members from a ride', function() {
+            let rideId = 1;
+            let memberIds = [1, 2];
+
+            this.collection.rides[rideId] = {
+                passengers: [1, 2]
+            };
+
+            this.collection.removeMembersFromRide(rideId, memberIds);
+
+            expect(this.collection.rides[rideId].passengers).to.deep.equal([]);
+        });
+
+        it('throws an error if a member is not a passenger in a ride', function() {
+            var _this = this;
+            let rideId = 1;
+            let memberIds = [1, 2, 3];
+
+            this.collection.rides[rideId] = {
+                passengers: [1, 2]
+            };
+
+            expect(function() {
+                _this.collection.removeMembersFromRide(rideId, memberIds);
+            }).to.throw(/Tried to remove a member/);
+        });
+    });
+});
--- src/browser/stores/collections/ride.js
+++ src/browser/stores/collections/ride.js
@@ -2,11 +2,12 @@ import RideRecord from './records/ride.js';
 import EventConstants from '../../constants/event.js';
 import RideConstants from '../../constants/ride.js';
 
-
 class RideCollection {
     constructor() {
-        // key: rideId
-        // value: Ride Record
+        /**
+         * key: rideId
+         * value: RideRecord
+         */
         this.rides = {};
     }
 
@@ -17,15 +18,24 @@ class RideCollection {
         return this.rides[id];
     }
 
+    /**
+     * Add raw rides, convert them into ride records, and add them to the rides map.
+     *
+     * @param {Array} rawRides A list of rides returned from the backend to convert to RideRecords.
+     */
     addRides(rawRides) {
-        for (let key in rawRides) {
-            if (rawRides.hasOwnProperty(key)) {
-                let rawRide = rawRides[key];
-                this.rides[rawRide.id] = new RideRecord(rawRide);
-            }
-        }
+        Object.keys(rawRides).forEach(key => {
+            let rawRide = rawRides[key];
+            this.rides[rawRide.id] = new RideRecord(rawRide);
+        });
     }
 
+    /**
+     * Adds members to the passengers array of a ride.
+     *
+     * @param {String} rideId The id of the ride that will receive the passengers.
+     * @param {String} membersIds The ids of the members that will be added as passengers.
+     */
     addMembersToRide(rideId, memberIds) {
         var ride = this.rides[rideId];
 
@@ -35,6 +45,14 @@ class RideCollection {
         });
     }
 
+    /**
+     * Removes members from the passengers array of a ride.
+     * Throws an error if any member id does not exist in the passengers array,
+     * because we should never enter that flow.
+     *
+     * @param {String} rideId The id of the ride that will lose the passengers.
+     * @param {String} membersIds The ids of the members that will be removed as passengers.
+     */
     removeMembersFromRide(rideId, memberIds) {
         var ride = this.rides[rideId];
 
@@ -43,6 +61,9 @@ class RideCollection {
 
             if (index > -1) {
                 ride.passengers.splice(index, 1);
+            } else {
+                // we should not ever enter this flow.
+                throw new Error('Tried to remove a member that did not belong to a ride.');
             }
         });
     }
