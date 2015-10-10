df1f89d97b2825e1d17cfc747a801f3b0c08d6a3
Scott Sperling <scsper@gmail.com>
2015-10-10 14:31:11 -0700

    Add sinon chai to unit test infrastructure.

--- test/setup/unit.js
+++ test/setup/unit.js
@@ -1,4 +1,17 @@
 require("babel/register");
+var sinon = require('sinon');
+var chai = require('chai');
+var sinonChai = require("sinon-chai");
+
+chai.use(sinonChai);
 
 // TODO: setup
 global.expect = require('chai').expect;
+
+beforeEach(function() {
+    this.sandbox = sinon.sandbox.create();
+});
+
+afterEach(function() {
+    this.sandbox.restore();
+});
